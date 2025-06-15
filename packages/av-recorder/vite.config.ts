fb1d5f7ae095ed76f99eb403866486b823915665
solidrock-do-it <jiuwei.stone@gmail.com>
2025-06-15 14:04:51 +0800

    chore: update build scripts and Vite configuration for output directories

--- packages/av-recorder/vite.config.ts
+++ packages/av-recorder/vite.config.ts
@@ -1,15 +1,25 @@
 import { resolve } from 'path';
-import { defineConfig } from 'vitest/config';
 import dts from 'vite-plugin-dts';
 import { externalizeDeps } from 'vite-plugin-externalize-deps';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  plugins: [dts({ rollupTypes: true }), externalizeDeps()],
+  plugins: [
+    dts({
+      rollupTypes: true,
+      include: ['src/**/*.ts'],
+      outDir: 'dist',
+      staticImport: true,
+    }),
+    externalizeDeps(),
+  ],
   build: {
     sourcemap: true,
     lib: {
       entry: resolve(__dirname, 'src/av-recorder.ts'),
       name: 'av-recorder',
+      formats: ['es', 'umd'],
+      fileName: (format) => `av-recorder.${format === 'es' ? 'js' : 'umd.cjs'}`,
     },
   },
   test: {
